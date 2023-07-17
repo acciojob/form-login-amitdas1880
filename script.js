f8f72e85e0284358969cee03d4e703dde5f649ce
Amit Kumar Das <102554819+amitdas1880@users.noreply.github.com>
2023-07-17 21:12:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+const fname =document.getElementById("f1").value;
+  const lname =document.getElementById("l2").value;
 
+  alert(fname +" "+lname);
+  
 }
